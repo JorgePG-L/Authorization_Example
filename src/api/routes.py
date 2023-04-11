3fb025a0374a8260f446545e942ac844ff659569
Jorge Piqueras García-Ligero <jorgepiqueras92@gmail.com>
2023-04-11 19:24:38 +0000

    feat:firstcommit

--- src/api/routes.py
+++ src/api/routes.py
@@ -4,8 +4,11 @@ This module takes care of starting the API Server, Loading the DB and Adding the
 from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
-
+import bcrypt
+from flask_jwt_extended import create_access_token, jwt_required, JWTManager
 api = Blueprint('api', __name__)
+
+
 
 
 @api.route('/hello', methods=['POST', 'GET'])
@@ -16,3 +19,28 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+@api.route('/user/register', methods=['POST'])
+def register():
+    body = request.get_json()
+    hashed = bcrypt.hashpw(body['password'].encode(),bcrypt.gensalt(14))
+    new_user = User(email = body['email'], password = hashed.decode(), is_active =True)
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify(new_user.serialize()), 201
+
+@api.route('/user/login', methods=['POST'])
+def login():
+    body = request.get_json()
+    user = User.query.filter_by( email = body['email']).one()
+    if bcrypt.checkpw(body['password'].encode(),user.password.encode()):
+        create_token = create_access_token(identity = user.serialize())
+        return jsonify(create_token),200
+    else:
+        return jsonify('no login')
+
+@api.route('/user/<int:id>', methods=['GET'])
+@jwt_required()
+def get_user_id(id):
+    user = User.query.get(id)
+    return jsonify(user.serialize()), 201
